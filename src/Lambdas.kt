a9deb306eef6c4c5f4bea01258b87ca0c4b72706
jdev <jdevcedres@gmail.com>
2026-06-27 23:13:04 +0100

    lambdas y funciones de orden superior

---
+++ src/Lambdas.kt
@@ -0,0 +1,40 @@
+fun main() {
+    /**
+     * Una lambda es, literalmente, una función sin nombre que puedes guardar en una variable, pasar como argumento a
+     * otra función, o devolver desde una función. Hasta ahora, toda función que escribiste tenía un nombre
+     * (fun saludar(){}). Una lambda es ese mismo concepto, pero más ligero y sin necesidad de declararla a aparte con fun
+     */
+
+    // Sintaxis básicas de una lambda
+
+    val saludar = { println("Hola!")}
+    saludar() // Hola!
+
+    val sumar = {a: Int, b: Int -> a + b}
+    println(sumar(3, 4)) // 7
+
+    /**
+     * Piezas clave:
+     *   Todo bloque va entre { } (no parentis)
+     *   Si la lambda recibe parámetros, se escribe al principio, seguidos de una flecha ->, y después el código
+     *   El resultado de la última línea dentro de la lambda es, automáticamente, lo que "devuelve"(no necesias escribir
+     *   un return)
+     */
+
+    // Comparación lado a lado con una función normal:
+
+    // Función normal (con nombre)
+    fun sumar2(a: Int, b: Int): Int {
+        return a + b
+    }
+    // Lambda (sin nombre, guardada en una variable)
+
+    val sumar3 = {a: Int, b: Int -> a + b}
+
+    println(sumar2(3, 4)) // 7
+    println(sumar3(3, 4))  //-- mismo resultado, dos formas distintas de escribirlo
+
+    /**
+     * FUNCIONES DE ORDEN SUPERIOR: funciones que reciben (o devuelven) otras funciones
+     */
+}
